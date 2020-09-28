c8ee51778cf0a1376502053df5454255a61bcf79
WillTimani <32140561+WillTimani@users.noreply.github.com>
2020-09-28 13:57:45 -0700

    Add files via upload

--- queries/timeline_queries.sql
+++ queries/timeline_queries.sql
@@ -1,14 +1,17 @@
+-- :name user_in_database :one
+select * from users where username = :username
+
 -- :name add_user_tweet :insert
 insert into tweets values (:username, :tweet, :currentTimestamp) 
 
 -- :name get_user_tweets :many
-select * from tweets where author like (:username)
+select * from tweets where author like (:username) ORDER BY posttimestamp DESC LIMIT 25
 
 -- :name get_followees :many
 select * from userfollowers where follower like (:username)
 
 -- :name get_followed_tweets :many
-select * from tweets where author in :followees
+select * from tweets where author in :followees ORDER BY posttimestamp DESC LIMIT 25
 
 -- :name get_all_tweets :many
-select * from tweets
+select * from tweets ORDER BY posttimestamp DESC LIMIT 25
